37db75ad33daa21c70f79bf197f0b20d8acf59f2
anhteo21499 <nguyenquocanh20499@gmail.com>
2022-08-16 00:00:57 +0700

    update

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -13,3 +13,24 @@ public class AdminController {
         return "Xin chao cac ban";
     }
 }
+//    pipeline {
+//        agent any
+//        tools{
+//            maven 'maven:3.6'
+//        }
+//        stages{
+//            stage('Build Maven'){
+//                steps{
+//                    git branch: 'main', credentialsId: 'TEST_CICD', url: 'https://github.com/anhteo21499/CICD_DOCKER.git'
+//                    sh "mvn clean install"
+//
+//                }
+//            }
+//
+//            stage('Build docker'){
+//                steps{
+//                    sh 'docker build -t demo1-0.0.1-SNAPSHOT .'
+//                }
+//            }
+//        }
+//    }
